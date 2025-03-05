310dcb05b0d28f23f43b1bfa964f27e711f3d139
kalebcory <150558486+kalebcory@users.noreply.github.com>
2025-03-05 17:30:25 -0600

    Added main package with MiddleEarthApp.java

---
+++ MiddleEartherManager/src/edu/mu/main/MiddleEarthApp.java
@@ -0,0 +1,34 @@
+package mu.edu.main;
+import mu.edu.Characters;
+import mu.edu.Manager;
+
+public class MiddleEarthApp {
+	public static void main(String[] args) {
+		MiddleEarthCouncil council = MiddleEarthCouncil.getInstance();
+		CharacterManager manager = council.getCharacterManager();
+		
+		manager.addCharacter(new Human("Kaleb", 100, 25));
+		manager.addCharacter(new Human("Cole", 100, 25));
+		manager.addCharacter(new Wizard("Supreet", 80, 50));
+				
+		MiddleEarthCharacter kaleb = manager.getCharacter("Kaleb");
+		MiddleEarthCharacter cole = manager.getCharacter("Cole");
+		MiddleEarthCharacter supreet = manager.getCharacter("Supreet");
+		
+		manager.displayAllCharacters();
+		
+		manager.deleteCharacter(kaleb);
+	
+		manager.updateCharacter(cole, "Cole", 100, 30);
+		
+		manager.displayAllCharacters();
+		
+		
+		if (cole != null && supreet != null) {
+			cole.attack(supreet);
+			supreet.attack(cole);
+		}
+			
+		manager.displayAllCharacters();
+	}
+}
