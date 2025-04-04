c5392b3d7492137c07cac11ef4f1d7801269ca81
kalebcory <150558486+kalebcory@users.noreply.github.com>
2025-04-04 16:59:52 -0500

    Created LegatoMidiEventFactoryAbstract for legato MIDI events

--- MyLittleMozartProject/src/mylittlemozart/edu/mu/factory/LegatoMidiEventFactoryAbstract.java
+++ MyLittleMozartProject/src/mylittlemozart/edu/mu/factory/LegatoMidiEventFactoryAbstract.java
@@ -1,5 +1,11 @@
 package mylittlemozart.edu.mu.factory;
 
-public class LegatoMidiEventFactoryAbstract {
-
+/**
+ * Abstract factory for legato MIDI events
+ */
+public class LegatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract {
+    @Override
+    public MidiEventFactory createFactory() {
+        return new LegatoMidiEventFactory();
+    }
 }
