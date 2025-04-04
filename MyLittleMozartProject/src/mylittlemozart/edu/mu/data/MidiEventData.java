cb7e625dd69959a68cd074e181a7d2ec3d6a2dfd
Cole Puls <colepuls@me.com>
2025-04-04 12:01:53 -0500

    variable initialization for MidiEventData

--- MyLittleMozartProject/src/mylittlemozart/edu/mu/data/MidiEventData.java
+++ MyLittleMozartProject/src/mylittlemozart/edu/mu/data/MidiEventData.java
@@ -1,5 +1,21 @@
 package mylittlemozart.edu.mu.data;
 
 public class MidiEventData {
-
+	private int startEndTick;
+	private int velocity;
+	private int note;
+	private int channel;
+	private int instrument;
+	private int noteOnOff;
+	
+	public MidiEventData(int startEndTick, int velocity, int note, int channel, int instrument, int noteOnOff) {
+		this.startEndTick = startEndTick;
+		this.velocity = velocity;
+		this.note = note;
+		this.channel = channel;
+		this.instrument = instrument;
+		this.noteOnOff = noteOnOff;
+	}
+	
+	
 }
