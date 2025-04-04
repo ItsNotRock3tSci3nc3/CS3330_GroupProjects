9b4684679f0634f7f723ec380f87b063ffe1d298
Cole Puls <colepuls@me.com>
2025-04-04 13:45:59 -0500

    started MidCsvParser implementation

--- MyLittleMozartProject/src/mylittlemozart/edu/mu/data/MidiCsvParser.java
+++ MyLittleMozartProject/src/mylittlemozart/edu/mu/data/MidiCsvParser.java
@@ -1,5 +1,17 @@
 package mylittlemozart.edu.mu.data;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MidiCsvParser {
-
+	
+	public static List<MidiEventData> parseCsv(String filePath) throws IOException {
+		
+		List<MidiEventData> eventList = new ArrayList<>();
+		
+		
+		
+	}
 }
