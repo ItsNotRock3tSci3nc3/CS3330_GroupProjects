1b90eb27f5c8c16d53aa0db9d2ecbe5b0149fb09
kalebcory <150558486+kalebcory@users.noreply.github.com>
2025-04-04 16:53:12 -0500

    Created MidiEventFactory for creating MIDI events

--- MyLittleMozartProject/src/mylittlemozart/edu/mu/factory/MidiEventFactory.java
+++ MyLittleMozartProject/src/mylittlemozart/edu/mu/factory/MidiEventFactory.java
@@ -1,5 +1,12 @@
-package mylittlemozart.edu.mu.factory;
+package midi.factory;
 
-public class MidiEventFactory {
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.InvalidMidiDataException;
 
+/**
+ * Interface for creating MIDI events
+ */
+public interface MidiEventFactory {
+    MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+    MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
