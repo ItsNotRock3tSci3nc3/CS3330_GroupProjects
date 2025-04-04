d3e2635b714097791a0f32120207e25a011de62e
kalebcory <150558486+kalebcory@users.noreply.github.com>
2025-04-04 16:58:11 -0500

    Created StaccatoMidiEventFactoryAbstract to shorten note duration

--- MyLittleMozartProject/src/mylittlemozart/edu/mu/factory/StaccatoMidiEventFactoryAbstract.java
+++ MyLittleMozartProject/src/mylittlemozart/edu/mu/factory/StaccatoMidiEventFactoryAbstract.java
@@ -1,5 +1,29 @@
-package mylittlemozart.edu.mu.factory;
+package midi.factory;
 
-public class StaccatoMidiEventFactoryAbstract {
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.InvalidMidiDataException;
 
+/**
+ * Staccato MIDI event factory that shortens note durations
+ */
+public class StaccatoMidiEventFactory implements MidiEventFactory {
+    private static final int DURATION_REDUCTION = 120;
+    
+    @Override
+    public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) 
+            throws InvalidMidiDataException {
+        ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        return new MidiEvent(message, tick);
+    }
+    
+    @Override
+    public MidiEvent createNoteOff(int tick, int note, int channel) 
+            throws InvalidMidiDataException {
+        ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        int adjustedTick = Math.max(0, tick - DURATION_REDUCTION);
+        return new MidiEvent(message, adjustedTick);
+    }
 }
