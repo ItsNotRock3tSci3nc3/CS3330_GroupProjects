e3acdae40514e7c0c7ca0bd6ffa8ea4ef1d3030c
Supreet <33377709+ItsNotRock3tSci3nc3@users.noreply.github.com>
2025-04-23 22:52:28 -0500

    Javadoc added for all methods

--- TDDproject/src/main/java/edu/mu/tddproject/player/Player.java
+++ TDDproject/src/main/java/edu/mu/tddproject/player/Player.java
@@ -10,23 +10,44 @@ public class Player {
     private Row currentRow;
     private Cell currentCell;
 
+    /**
+     * Player constructor, initializes variables
+     * @param currentRow: starting row
+     * @param currentCell: starting cell
+     */
     public Player(Row currentRow, Cell currentCell) {
         this.currentRow = currentRow;
         this.currentCell = currentCell;
     }
 
+    /**
+     * getter for current row
+     * @return currentRow
+     */
     public Row getCurrentRow() {
         return currentRow;
     }
 
+    /**
+     * getter for current cell
+     * @return currentCell
+     */
     public Cell getCurrentCell() {
         return currentCell;
     }
-
+    
+    /**
+     * setter for current row
+     * @param row object
+     */
     public void setCurrentRow(Row row) {
         this.currentRow = row;
     }
 
+    /**
+     * setter for current cell
+     * @param cell object
+     */
     public void setCurrentCell(Cell cell) {
         this.currentCell = cell;
     }
