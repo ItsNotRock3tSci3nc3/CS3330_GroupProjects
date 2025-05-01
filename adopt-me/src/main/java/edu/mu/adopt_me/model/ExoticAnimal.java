079c94bea5704efea6e31a0e173e025603d448e0
Cole Puls <colepuls@me.com>
2025-05-01 11:31:43 -0500

    Added type pet classes along with Exotic animal adpater

---
+++ adopt-me/src/main/java/edu/mu/adopt_me/model/ExoticAnimal.java
@@ -0,0 +1,9 @@
+package edu.mu.adopt_me.model;
+
+public class ExoticAnimal {
+	public String uniqueId;
+	public String animalName;
+	public String category;
+	public String subSpecies;
+	public int yearsOld;
+}
---
+++ adopt-me/src/main/java/edu/mu/adopt_me/model/Rabbit.java
@@ -0,0 +1,10 @@
+package edu.mu.adopt_me.model;
+
+/**
+ * A rabbit that can be adopted.
+ */
+public class Rabbit extends Pet {
+	public Rabbit(String id, String species, String name, int age) {
+		super(id, "Rabbit", species, name, age);
+	}
+}
