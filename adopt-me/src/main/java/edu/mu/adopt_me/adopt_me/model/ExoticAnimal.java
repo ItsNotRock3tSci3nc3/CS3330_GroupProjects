4615b5799c8645f1eb1bd0e298b926e6df617e3e
Cole Puls <colepuls@me.com>
2025-05-01 11:30:27 -0500

    Delete adopt-me/src/main/java/edu/mu/adopt_me/adopt_me directory

--- adopt-me/src/main/java/edu/mu/adopt_me/adopt_me/model/ExoticAnimal.java
+++
@@ -1,9 +0,0 @@
-package edu.mu.adopt_me.model;
-
-public class ExoticAnimal {
-	public String uniqueId;
-	public String animalName;
-	public String category;
-	public String subSpecies;
-	public int yearsOld;
-}
